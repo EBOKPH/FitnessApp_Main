0c96f8c8914616b4cf0c7655fa845a48199592b4
ArmaanGilani <armangilani777@gmail.com>
2025-07-28 23:06:26 +0530

    Updated Code

--- lib/screens/add_alarm_screen.dart
+++ lib/screens/add_alarm_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AddAlarmScreen extends StatefulWidget {
   const AddAlarmScreen({super.key});
@@ -12,11 +13,121 @@ class _AddAlarmScreenState extends State<AddAlarmScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
 
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Add Alarm",
+          style: GoogleFonts.poppins(
+            textStyle: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black
+            ),
+          ),
+        ),
+        leading: Padding(
+          padding: EdgeInsets.fromLTRB(16.0, 0.0, 0.0, 10.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey.shade300,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.arrow_back_ios_new_rounded, color: Colors.black,),
+            ),
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 16.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey.shade300,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.more_horiz_rounded, color: Colors.black,),
+              ),
+            ),
+          ),
+        ],
+      ),
+
       body: SafeArea(
           child: Column(
             children: [
 
-              Text("data"),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: Colors.grey[200],
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+
+                        Row(
+                          children: [
+
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            Icon(
+                              Icons.bed_sharp,
+                              size: 36,
+                            ),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            Text("Bedtime",
+                              style: GoogleFonts.poppins(
+                                textStyle: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.black
+                                ),
+                              ),
+                            ),
+
+                          ],
+                        ),
+
+                        Row(
+                          children: [
+
+                            Text("09:00 PM",
+                              style: GoogleFonts.poppins(
+                                textStyle: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.black
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            Icon(
+                              Icons.arrow_forward_ios_outlined,
+                              size: 22,
+                            ),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+
+                          ],
+                        ),
+
+                      ],
+                    ),
+                  ),
+                ),
+              ),
 
             ],
           ),
