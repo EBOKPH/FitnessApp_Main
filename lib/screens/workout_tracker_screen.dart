eee80e06fba6971f52c6408e0a6c966f51a6cdf0
ArmaanGilani <armangilani777@gmail.com>
2025-07-29 22:12:27 +0530

    Updated Code

--- lib/screens/workout_tracker_screen.dart
+++ lib/screens/workout_tracker_screen.dart
@@ -1,4 +1,6 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WorkoutTrackerScreen extends StatefulWidget {
   const WorkoutTrackerScreen({super.key});
@@ -8,14 +10,118 @@ class WorkoutTrackerScreen extends StatefulWidget {
 }
 
 class _WorkoutTrackerScreenState extends State<WorkoutTrackerScreen> {
+
+  List<Map<String, double>> chartData = [
+    {'x': 0, 'y': 75},
+    {'x': 1, 'y': 78},
+    {'x': 2, 'y': 72},
+    {'x': 3, 'y': 80},
+    {'x': 4, 'y': 74},
+    {'x': 5, 'y': 79},
+    {'x': 6, 'y': 77},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
 
-      appBar: AppBar(),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Workout Tracker",
+          style: GoogleFonts.poppins(
+            textStyle: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black
+            ),
+          ),
+        ),
+        leading: Padding(
+          padding: EdgeInsets.fromLTRB(16.0, 0.0, 0.0, 10.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey.shade300,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.arrow_back_ios_new_rounded, color: Colors.black,),
+            ),
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 16.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey.shade300,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.more_horiz_rounded, color: Colors.black,),
+              ),
+            ),
+          ),
+        ],
+      ),
 
-      body: Center(
-        child: Text("WorkoutTrackerScreen!!!"),
+      body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+
+                Padding(
+                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  child: SizedBox(
+                    height: 300,
+                    child: LineChart(
+                      LineChartData(
+                        titlesData: FlTitlesData(
+                          leftTitles: AxisTitles(
+                            sideTitles: SideTitles(showTitles: true),
+                          ),
+                          bottomTitles: AxisTitles(
+                            sideTitles: SideTitles(
+                              showTitles: true,
+                              interval: 1,
+                              getTitlesWidget: (value, meta) => Text(value.toInt().toString()),
+                            ),
+                          ),
+                        ),
+                        borderData: FlBorderData(
+                          show: true,
+                          border: Border.all(color: Colors.grey, width: 1),
+                        ),
+                        lineBarsData: [
+                          LineChartBarData(
+                            spots: [
+                              FlSpot(0, 3),
+                              FlSpot(1, 1),
+                              FlSpot(2, 4),
+                              FlSpot(3, 2),
+                              FlSpot(4, 5),
+                            ],
+                            isCurved: true,
+                            color: Colors.blue,
+                            barWidth: 3,
+                            dotData: FlDotData(show: true),
+                            belowBarData: BarAreaData(
+                              show: true,
+                              color: Colors.blue.withOpacity(0.2),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+
+              ],
+            ),
+          ),
       ),
 
     );
