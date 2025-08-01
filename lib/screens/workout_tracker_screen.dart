dff6249e0032c6e0195f12a120f9085ac1642d0a
ArmaanGilani <armangilani777@gmail.com>
2025-08-01 21:42:30 +0530

    Updated Code

--- lib/screens/workout_tracker_screen.dart
+++ lib/screens/workout_tracker_screen.dart
@@ -210,8 +210,8 @@ class _WorkoutTrackerScreenState extends State<WorkoutTrackerScreen> {
                                   borderRadius: BorderRadius.circular(44.0),
                                 ),
                                 child: Icon(
-                                    Icons.person,
-                                    size: 46.0
+                                    Icons.sports_gymnastics,
+                                    size: 46.0,
                                 ),
                               ),
                               Padding(
@@ -252,6 +252,92 @@ class _WorkoutTrackerScreenState extends State<WorkoutTrackerScreen> {
                           ),
 
                         ],
+                      ),
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.only(left: 20.0, top: 20.0, right: 20.0),
+                  child: Material(
+                    elevation: 1,
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: Colors.white,
+                    child: Padding(
+                      padding: EdgeInsets.all(12.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+
+                          Row(
+                            children: [
+
+                              Container(
+                                width: 65,
+                                height: 65,
+                                decoration: BoxDecoration(
+                                  color: Color(0XFFE9F0FF),
+                                  borderRadius: BorderRadius.circular(44.0),
+                                ),
+                                child: Icon(
+                                    Icons.sports_gymnastics,
+                                    size: 46.0,
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 10.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+
+                                    Text("Upperbody Workout",
+                                      style: GoogleFonts.poppins(
+                                        textStyle: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.black
+                                        ),
+                                      ),
+                                    ),
+                                    Text("June 05, 02:00pm",
+                                      style: GoogleFonts.poppins(
+                                        textStyle: TextStyle(
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.w600,
+                                            color: Colors.grey
+                                        ),
+                                      ),
+                                    ),
+
+                                  ],
+                                ),
+                              ),
+
+                            ],
+                          ),
+
+                          Switch(
+                            value: false,
+                            onChanged: (value) {},
+                          ),
+
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 20.0, top: 20.0),
+                    child: Text("What Do You Want To Train",
+                      style: GoogleFonts.poppins(
+                        textStyle: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black
+                        ),
                       ),
                     ),
                   ),
