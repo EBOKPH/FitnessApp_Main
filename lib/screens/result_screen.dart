2e6cce0e1e62b25792680064606689153d3f75d4
ARMAN GILANI <armangilani777@gmail.com>
2025-08-18 22:09:19 +0530

    Updated Code

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -9,6 +10,17 @@ class ResultScreen extends StatefulWidget {
 }
 
 class _ResultScreenState extends State<ResultScreen> {
+
+  List<Map<String, double>> chartData = [
+    {'x': 0, 'y': 75},
+    {'x': 1, 'y': 78},
+    {'x': 2, 'y': 72},
+    {'x': 3, 'y': 80},
+    {'x': 4, 'y': 74},
+    {'x': 5, 'y': 79},
+    {'x': 6, 'y': 77},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -426,12 +438,135 @@ class _ResultScreenState extends State<ResultScreen> {
                       ),
                     ),
 
-                    Column(
-                      children: [
+                    SingleChildScrollView(
+                      child: Column(
+                        children: [
 
-                        //
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                            child: SizedBox(
+                              height: 300,
+                              child: LineChart(
+                                LineChartData(
+                                  titlesData: FlTitlesData(
+                                    leftTitles: AxisTitles(
+                                      sideTitles: SideTitles(showTitles: true),
+                                    ),
+                                    bottomTitles: AxisTitles(
+                                      sideTitles: SideTitles(
+                                        showTitles: true,
+                                        interval: 1,
+                                        getTitlesWidget: (value, meta) => Text(value.toInt().toString()),
+                                      ),
+                                    ),
+                                  ),
+                                  borderData: FlBorderData(
+                                    show: true,
+                                    border: Border.all(color: Colors.grey, width: 1),
+                                  ),
+                                  lineBarsData: [
+                                    LineChartBarData(
+                                      spots: [
+                                        FlSpot(0, 3),
+                                        FlSpot(1, 1),
+                                        FlSpot(2, 4),
+                                        FlSpot(3, 2),
+                                        FlSpot(4, 5),
+                                      ],
+                                      isCurved: true,
+                                      color: Colors.blue,
+                                      barWidth: 3,
+                                      dotData: FlDotData(show: true),
+                                      belowBarData: BarAreaData(
+                                        show: true,
+                                        color: Colors.blue.withOpacity(0.2),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
 
-                      ],
+                          Padding(
+                            padding: EdgeInsets.only(left: 20.0, top: 20.0, right: 20.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+
+                                Text("May",
+                                  style: GoogleFonts.poppins(
+                                    textStyle: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                                Text("June",
+                                  style: GoogleFonts.poppins(
+                                    textStyle: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+
+                              ],
+                            ),
+                          ),
+
+
+
+
+                          
+
+
+
+
+
+
+                          Padding(
+                            padding: EdgeInsets.only(left: 20.0, top: 30.0, right: 20.0, bottom: 30.0),
+                            child: SizedBox(
+                              height: 55,
+                              width: double.infinity,
+                              child: DecoratedBox(
+
+                                decoration: BoxDecoration(
+                                  gradient: LinearGradient(
+                                    colors: [Color(0xFF97B3FE), Color(0xFF9AC2FF)],
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomRight,
+                                  ),
+                                  borderRadius: BorderRadius.circular(30.0),
+
+                                ),
+                                child: ElevatedButton(
+
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.transparent,
+                                    shadowColor: Colors.transparent,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8.0),
+                                    ),
+
+                                  ),
+                                  onPressed: () {},
+                                  child: Text(
+                                    "Back to Home",
+                                    style: TextStyle(
+
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.white,
+
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+
+                        ],
+                      ),
                     ),
 
                   ],
